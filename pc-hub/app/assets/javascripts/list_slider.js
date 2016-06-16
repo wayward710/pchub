0786cc88f6fe70c49245f8e71534e776051f8219
Izabela Cardoso <cizabelacristina@gmail.com>
2016-06-15 23:42:59 -0300

    Issue #12: Adding web apps slider

---
+++ pc-hub/app/assets/javascripts/list_slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $('.carousel').carousel({
+        interval: false
+    }); 
+    
+    $('.carousel .item').each(function(){
+      var next = $(this).next();
+      
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<2;i++) {
+        next=next.next();
+        
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
+});
